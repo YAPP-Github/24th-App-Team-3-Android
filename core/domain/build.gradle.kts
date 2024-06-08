da30d941ea187885fa07c5b4a780d5b0cb0a4080
soopeach <hsjeon584@gmail.com>
2024-06-08 12:49:36 +0900

    chore: domain 모듈이 data 모듈을 참조하도록 설정

--- core/domain/build.gradle.kts
+++ core/domain/build.gradle.kts
@@ -34,6 +34,8 @@ android {
 
 dependencies {
 
+    implementation(project(":core:data"))
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
