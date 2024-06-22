6b0dfd4d647b4d67609313d9ec1a332a61aea8d1
soopeach <hsjeon584@gmail.com>
2024-06-22 17:02:57 +0900

    Feat: ShowPot 한국어 텍스트 컴포저블 세팅

---
+++ core/designsystem/src/main/java/com/alreadyoccupiedseat/designsystem/typo/korean/ShowPotKoreanText_B2_SemiBold.kt
@@ -0,0 +1,12 @@
+package com.alreadyoccupiedseat.designsystem.typo.korean
+
+import androidx.compose.runtime.Composable
+import com.alreadyoccupiedseat.designsystem.ShowPotTypography
+import com.alreadyoccupiedseat.designsystem.typo.ShowPotBaseText
+
+@Composable
+fun ShowPotKoreanText_B2_SemiBold(
+    text: String,
+) {
+    ShowPotBaseText(text = text, style = ShowPotTypography.Korean.B2_semiBold)
+}
