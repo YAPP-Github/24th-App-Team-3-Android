e79ac2a571a2813fcc30544bd55298d34f33d88b
soopeach <hsjeon584@gmail.com>
2024-06-22 16:45:04 +0900

    Feat: ShowPotTypography 세팅

--- core/designsystem/src/main/java/com/alreadyoccupiedseat/designsystem/Type.kt
+++ core/designsystem/src/main/java/com/alreadyoccupiedseat/designsystem/Type.kt
@@ -2,6 +2,7 @@ package com.alreadyoccupiedseat.designsystem
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
@@ -32,3 +33,107 @@ val Typography = Typography(
     )
     */
 )
+
+val PretendardRegularFontFamily = FontFamily(Font(R.font.pretendard_regular))
+val PretendardSemiBoldFontFamily = FontFamily(Font(R.font.pretendard_semibold))
+val OswaldRegularFontFamily = FontFamily(Font(R.font.oswald_regular))
+
+// Todo: Color
+sealed class ShowPotTypography {
+    data object Korean {
+
+        val H0 = TextStyle(
+            fontFamily = PretendardSemiBoldFontFamily,
+            fontWeight = FontWeight.SemiBold,
+            fontSize = 24.sp,
+        )
+
+        val H1 = TextStyle(
+            fontFamily = PretendardSemiBoldFontFamily,
+            fontWeight = FontWeight.SemiBold,
+            fontSize = 20.sp,
+        )
+
+        val H2 = TextStyle(
+            fontFamily = PretendardSemiBoldFontFamily,
+            fontWeight = FontWeight.SemiBold,
+            fontSize = 18.sp,
+        )
+
+        val B1_semiBold = TextStyle(
+            fontFamily = PretendardSemiBoldFontFamily,
+            fontWeight = FontWeight.SemiBold,
+            fontSize = 16.sp,
+        )
+
+        val B2_semiBold = TextStyle(
+            fontFamily = PretendardSemiBoldFontFamily,
+            fontWeight = FontWeight.SemiBold,
+            fontSize = 14.sp,
+        )
+
+        val B3_semiBold = TextStyle(
+            fontFamily = PretendardSemiBoldFontFamily,
+            fontWeight = FontWeight.SemiBold,
+            fontSize = 12.sp,
+        )
+
+        val B1_regular = TextStyle(
+            fontFamily = PretendardRegularFontFamily,
+            fontWeight = FontWeight.Normal,
+            fontSize = 16.sp,
+        )
+
+        val B2_regular = TextStyle(
+            fontFamily = PretendardRegularFontFamily,
+            fontWeight = FontWeight.Normal,
+            fontSize = 14.sp,
+        )
+
+        val B3_regular = TextStyle(
+            fontFamily = PretendardRegularFontFamily,
+            fontWeight = FontWeight.Normal,
+            fontSize = 12.sp,
+        )
+    }
+
+    data object English {
+
+        val H0 = TextStyle(
+            fontFamily = OswaldRegularFontFamily,
+            fontWeight = FontWeight.Normal,
+            fontSize = 30.sp,
+        )
+
+        val H1 = TextStyle(
+            fontFamily = OswaldRegularFontFamily,
+            fontWeight = FontWeight.Normal,
+            fontSize = 24.sp,
+        )
+
+        val H2 = TextStyle(
+            fontFamily = OswaldRegularFontFamily,
+            fontWeight = FontWeight.Normal,
+            fontSize = 22.sp,
+        )
+
+        val H3 = TextStyle(
+            fontFamily = OswaldRegularFontFamily,
+            fontWeight = FontWeight.Normal,
+            fontSize = 20.sp,
+        )
+
+        val H4 = TextStyle(
+            fontFamily = OswaldRegularFontFamily,
+            fontWeight = FontWeight.Normal,
+            fontSize = 18.sp,
+        )
+
+        val H5 = TextStyle(
+            fontFamily = OswaldRegularFontFamily,
+            fontWeight = FontWeight.Normal,
+            fontSize = 16.sp,
+        )
+
+    }
+}
