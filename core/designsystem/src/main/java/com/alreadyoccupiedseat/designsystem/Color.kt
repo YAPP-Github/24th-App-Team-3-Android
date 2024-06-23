ee8a116360320f6531cda00d8a53040754092308
hyeonseok <startwo09@gmail.com>
2024-06-23 11:42:49 +0900

    Feat: ShowPot 색상 값 추가

--- core/designsystem/src/main/java/com/alreadyoccupiedseat/designsystem/Color.kt
+++ core/designsystem/src/main/java/com/alreadyoccupiedseat/designsystem/Color.kt
@@ -2,10 +2,21 @@ package com.alreadyoccupiedseat.designsystem
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val MainRed = Color(0xFFF05F1F)
+val MainGreen = Color(0xFF3F6870)
+val MainYellow = Color(0xFFFFCA2A)
+val MainBlue = Color(0xFF6692E9)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Object, Line, Text Colors
+val White = Color(0xFFFFFFFF)
+val Gray100 = Color(0xFFC3C3C6)  // text color
+val Gray200 = Color(0xFF9E9E03)  // text color
+val Gray300 = Color(0xFF62646C) // Tab bar
+val Gray400 = Color(0xFF454751)
+val Gray500 = Color(0xFF2C2E34)  // card
+val Gray600 = Color(0xFF202026)  // text color
+val Gray700 = Color(0xFF17171B)    // BG
+val Gray800 = Color(0xFF101012)   // Divider text color
+
+// Error Color
+val ErrorRed = Color(0xFFFF5E5E)
