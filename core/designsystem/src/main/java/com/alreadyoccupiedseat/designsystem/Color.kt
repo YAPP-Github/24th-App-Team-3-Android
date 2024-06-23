9a0c2a65fe5e063461ffac15dc81d08d2717ba03
hyeonseok <startwo09@gmail.com>
2024-06-23 19:54:35 +0900

    Chore: Showpot Color 추가

--- core/designsystem/src/main/java/com/alreadyoccupiedseat/designsystem/Color.kt
+++ core/designsystem/src/main/java/com/alreadyoccupiedseat/designsystem/Color.kt
@@ -3,20 +3,28 @@ package com.alreadyoccupiedseat.designsystem
 import androidx.compose.ui.graphics.Color
 
 val MainRed = Color(0xFFF05F1F)
-val MainGreen = Color(0xFF3F6870)
+val MainGreen = Color(0xFF288B5A)
+
 val MainYellow = Color(0xFFFFCA2A)
 val MainBlue = Color(0xFF6692E9)
 
 // Object, Line, Text Colors
 val White = Color(0xFFFFFFFF)
-val Gray100 = Color(0xFFC3C3C6)  // text color
-val Gray200 = Color(0xFF9E9E03)  // text color
-val Gray300 = Color(0xFF62646C) // Tab bar
+val Gray100 = Color(0xFFC3C3C6)
+val Gray200 = Color(0xFF9EA0A3)
+val Gray300 = Color(0xFF62646C)
 val Gray400 = Color(0xFF454751)
-val Gray500 = Color(0xFF2C2E34)  // card
-val Gray600 = Color(0xFF202026)  // text color
-val Gray700 = Color(0xFF17171B)    // BG
-val Gray800 = Color(0xFF101012)   // Divider text color
+val Gray500 = Color(0xFF2C2E34)
+val Gray600 = Color(0xFF202026)
+val Gray700 = Color(0xFF17171B)
+val Gray800 = Color(0xFF101012)
 
 // Error Color
 val ErrorRed = Color(0xFFFF5E5E)
+
+// Chip Color
+val YES24 = Color(0xFF007FFF)
+val Interpark = Color(0xFF8E43E6)
+val Melon = Color(0xFF0FAE2F)
+val Wemakeprice = Color(0xFFE7312E)
+
