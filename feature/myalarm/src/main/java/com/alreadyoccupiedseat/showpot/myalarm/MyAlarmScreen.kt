2d016aac130ef626c0b39b8c6e546eac96dbd95f
hyeonseok <startwo09@gmail.com>
2024-08-06 23:24:19 +0900

    Feat: MyAlarmScreen 컴포저블 함수 추가

---
+++ feature/myalarm/src/main/java/com/alreadyoccupiedseat/showpot/myalarm/MyAlarmScreen.kt
@@ -0,0 +1,9 @@
+package com.alreadyoccupiedseat.showpot.myalarm
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun MyAlarmScreen(modifier: Modifier = Modifier) {
+
+}
